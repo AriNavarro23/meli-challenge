b0ea9700df3193a843c79087e7c127efb96a9a9e
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-07-28 01:07:00 -0300

    change page app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import {AppProps} from "next/app";
-import {ChakraProvider, Stack, Image, Input, IconButton} from "@chakra-ui/react";
+import {ChakraProvider, Stack, Image, Input, IconButton, Box} from "@chakra-ui/react";
 
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  function handleSubmit(event: React.FormEvent<HTMLDivElement>){
+    event.preventDefault();
+
+    console.log(event.target['query'].value);
+    
+  }
+
   return( 
   <ChakraProvider>
     <Stack>
       <Stack bgColor="yellow.400" direction="row" padding={4} spacing={6}>
         <Image src="/logo.png"/>
-        <Stack direction="row" spacing={0} width="100%">
-          <Input bgColor="white"/>
-          <IconButton 
-            aria-label='Search database' 
-            icon={<img src="https://icongr.am/feather/search.svg?size=24&color=#333" />} />
-        </Stack>
+        <Box as="form" width="100%" onSubmit={handleSubmit} >
+          <Stack direction="row" spacing={0} width="100%">
+            <Input bgColor="white" roundedRight={0}/>
+            <IconButton 
+              aria-label='Search database' 
+              icon={<img src="https://icongr.am/feather/search.svg?size=20&color=#666 " />} 
+              roundedLeft={0}
+            />
+          </Stack>
+        </Box>
       </Stack>
       <Component {...pageProps} />
     </Stack>
